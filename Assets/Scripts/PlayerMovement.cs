6258346483451acc88328e4106930ee28de0624a
KyoShred <lucas.salaun2@gmail.com>
2024-10-09 09:37:57 +0200

    Add new animation files

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float input;
     public float stepDistance;   // Distance parcourue à chaque "pas"
     private string lastKey = "";      // Stocke la dernière touche appuyée
+    private Animator anim;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,11 @@ public class PlayerMovement : MonoBehaviour
         if (playerRb == null)
         {
             playerRb = GetComponent<Rigidbody2D>();
+        }
+        anim = GetComponent<Animator>();
+        if (anim == null)
+        {
+            Debug.LogError("Animator component not found!");
         }
     }
 
@@ -32,16 +38,19 @@ public class PlayerMovement : MonoBehaviour
             StepForward();
             Debug.Log("Yes !");
         }
-
-        
+        anim.SetBool("Run", false);
     }
 
     // Fonction pour avancer d'un pas
-    void StepForward()
+    void StepForward() 
     {
         Debug.Log("StepForward called");
         // Déplace le personnage vers l'avant d'une certaine distance
         playerRb.MovePosition(playerRb.position + new Vector2(stepDistance, 0));
+        if (anim != null)
+        {
+            anim.SetBool("Run", true);
+            Debug.Log("Run animation set to true");
+        }
     }
-
 }
