c49518faa230914ff3abc51fb6c03dc472f8bb70
KyoShred <lucas.salaun2@gmail.com>
2024-10-10 09:40:27 +0200

    Add new assets and scripts for background parallax functionality

---
+++ Assets/Scripts/Background/ParallaxLayer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+ 
+[ExecuteInEditMode]
+public class ParallaxLayer : MonoBehaviour
+{
+    public float parallaxFactor;
+ 
+    public void Move(float delta)
+    {
+        Vector3 newPos = transform.localPosition;
+        newPos.x -= delta * parallaxFactor;
+ 
+        transform.localPosition = newPos;
+    }
+ 
+}
+ 
