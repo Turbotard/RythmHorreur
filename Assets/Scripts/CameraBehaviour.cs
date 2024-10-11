c9629646213a0db898af999ace877cae97b300c4
KyoShred <lucas.salaun2@gmail.com>
2024-10-11 11:43:22 +0200

    Replacement of player object and addition of camera vertical offset

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -8,6 +8,7 @@ public class CameraBehaviour : MonoBehaviour
     public float minScore = 0f;                   // Le score minimum
     public float maxRightOffset = 5f;             // Distance maximale à droite de l'écran
     public float maxLeftOffset = 5f;              // Distance maximale à gauche de l'écran
+    public float maxUpOffset = 5f;               
     private Vector3 offset = new Vector3(0, 0, -10); // Position initiale de la caméra par rapport au joueur
     public PlayerMovement movement;               // Référence au mouvement du joueur
 
@@ -22,7 +23,7 @@ public class CameraBehaviour : MonoBehaviour
         float horizontalOffset = Mathf.Lerp(maxRightOffset, -maxLeftOffset, Mathf.InverseLerp(minScore, maxScore, playerScore));
 
         // Position cible de la caméra avec un décalage en fonction du score
-        Vector3 desiredPosition = player.position + offset + new Vector3(horizontalOffset, 0, 0);
+        Vector3 desiredPosition = player.position + offset + new Vector3(horizontalOffset, maxUpOffset, 0);
 
         // Appliquer un mouvement lissé vers la position cible
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
